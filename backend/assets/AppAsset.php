74ab3c1044ab3203b0f4f8f5edad5eef1d2366a5
thanhtamcntt12 <thanhtamcntt15@gmail.com>
2017-07-29 08:50:56 +0700

    admin-templete

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,20 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'vendors/bootstrap/dist/css/bootstrap.min.css',
+        'vendors/font-awesome/css/font-awesome.min.css',
+        'vendors/nprogress/nprogress.css',
+        'vendors/google-code-prettify/bin/prettify.min.css',
+        'build/css/custom.min.css',
     ];
     public $js = [
+        'vendors/jquery/dist/jquery.min.js',
+        'vendors/bootstrap/dist/js/bootstrap.min.js',
+        'vendors/fastclick/lib/fastclick.js',
+        'vendors/nprogress/nprogress.js',
+        'vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js',
+        'vendors/jquery.hotkeys/jquery.hotkeys.js',
+        'vendors/google-code-prettify/src/prettify.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
